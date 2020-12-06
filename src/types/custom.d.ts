4d379b33d884968b84161b642cb9d370690019d9
Mitchell <mitchell@demarcosoftware.com>
2020-12-06 01:41:59 -0800

    Left comment describing insanity

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,5 +1,6 @@
 declare namespace Express {
   interface Request {
+    // Fixes bug in VScode that keeps code from being autocompleted
     context: import("./context").Context,
     smoothieId?: string
   }
